274f457727ebcbf6f4ae0bae959d25f6452edd0a
Volodymyr Rud <volodymyr.rud.ki.2021@lpnu.ua>
2024-12-21 20:03:42 +0200

    new hope

--- scripts/build_controller.sh
+++ scripts/build_controller.sh
@@ -1,11 +1,18 @@
-    # Build Controller
+    # Build test on controller
 #        rm -rf ../Deploy/Controller/results_of_build
 #        mkdir -p ../Deploy/Controller/results_of_build
         cd ../RPS_Controller
         source /home/rud/esp/esp-idf/export.sh
 #        cmake -S . -B build-tests -DBUILD_TESTS=ON
+        echo $PWD
+        cd RPS_Controller
         idf.py fullclean
         idf.py -D BUILD_TESTS=ON build-tests
 
 # idf.py merge-bin
+# Set the components to include the tests for.
+# This can be overridden from CMake cache:
+# - when invoking CMake directly: cmake -D TEST_COMPONENTS="xxxxx" ..
+# - when using idf.py: idf.py -T xxxxx build
+#
         
